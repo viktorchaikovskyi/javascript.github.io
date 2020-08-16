66985d2fe160f682ffdca95dbf3cac6e533cd36a
Viktor <chaykin9@gmail.com>
2020-08-16 19:46:35 +0300

    second

--- script copy.js
+++ script copy.js
@@ -4,22 +4,25 @@
 
 /** Цикли */
 
-let num = 50;
-    // while (num < 55) {
-    //     console.log(num);
-    //     mun++;
-    // }
+// let num = 50;
+//     // while (num < 55) {
+//     //     console.log(num);
+//     //     mun++;
+//     // }
 
-do {
-    console.log(num);
-    num++;
-}
-while (num < 55);
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);
 
-for (let i = 1; i < 8; i++) {
-    if (i == 6) {
-        break                   //перерве цикл
-        /** continue            //перерве цикл в даному значені і продовжить його далі */                 
-    }
-    console.log(i);
-}
+// for (let i = 1; i < 8; i++) {
+//     if (i == 6) {
+//         break                   //перерве цикл
+//         /** continue            //перерве цикл в даному значені і продовжить його далі */                 
+//     }
+//     console.log(i);
+// }
+
+//* Функции */
+
